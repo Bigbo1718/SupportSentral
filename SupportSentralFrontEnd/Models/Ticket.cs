76edf82d6b3637a84f363a4f8db26d8bfff90beb
boboateng <eboateng@thenulevel.com>
2025-07-14 11:42:47 -0400

    Update to add initial frontend project into repo

---
+++ SupportSentralFrontEnd/Models/Ticket.cs
@@ -0,0 +1,18 @@
+namespace SupportSentralFrontEnd.Models;
+
+public class Tickets
+{
+    public int Id { get; set; }
+    
+    public required string Title { get; set; }
+    
+    public required string Description { get; set; }
+    
+    public int StatusId { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime? UpdatedAt { get; set; }
+    
+    public required string AssignedTo { get; set; }
+}
